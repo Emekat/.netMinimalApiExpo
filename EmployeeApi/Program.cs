cb0b90ac190fa377456549f8a5adcd5ef60f3c87
Chukwuemeka Torti <emekatorti@gmail.com>
2024-12-24 03:42:43 -0500

    Switched endpoint return types to TypedResults

--- EmployeeApi/Program.cs
+++ EmployeeApi/Program.cs
@@ -16,51 +16,37 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-EmployeeManager.Create(new Employee
-{
-    Id = 1,
-    Name = "John Doe",
-    City = "New York",
-    Region = "NY",
-    PostalCode = "10001",
-    Address = "123 Main Street",
-    Country = "USA",
-    Phone = "123-456-7890",
-    Salary = 100000
-});
 
-app.MapGet("/employees", () => Results.Ok(EmployeeManager.GetEmployees())).WithName("GetEmployees");    
-
-app.MapGet("/employees/{id}", (int id) => Results.Ok(EmployeeManager.Get(id))).WithName("GetEmployee");
+app.MapGet("/employees/{id}", (int id) => TypedResults.Ok(EmployeeManager.Get(id))).WithName("GetEmployee");
 
 app.MapPost("/employees", (Employee employee) =>
 {
     EmployeeManager.Create(employee);
-    return Results.Created($"/employees/{employee.Id}", employee);
+    return TypedResults.Created($"/employees/{employee.Id}", employee);
 }).WithName("CreateEmployee");
 
 app.MapPut("/employees/{id}", (int id, Employee employee) =>
 {
     EmployeeManager.Update(id, employee);
-    return Results.Ok(employee);
+    return TypedResults.Ok(employee);
 }).WithName("UpdateEmployeeById");
 
 app.MapPut("/employees", (Employee employee) =>
 {
     EmployeeManager.UpdateEmployee(employee);
-    return Results.Ok(employee);
+    return TypedResults.Ok(employee);
 }).WithName("UpdateEmployee");
 
 app.MapPatch("/employees/{id}/name", (int id, string name) =>
 {
     EmployeeManager.ChangeName(id, name);
-    return Results.Ok();
+    return TypedResults.Ok();
 }).WithName("ChangeEmployeeName");
 
 app.MapDelete("/employees/{id}", (int id) =>
 {
     EmployeeManager.DeleteEmployee(id);
-    return Results.Ok();
+    return TypedResults.Ok();
 }).WithName("DeleteEmployee");
 
 
